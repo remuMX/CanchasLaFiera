7ea51d8d41515ff6b4314cda51aa070ecdb1bc89
Diego Sait Grande Camacho <remumex16dansei@gmail.com>
2018-09-01 15:16:33 -0500

    Basic Structure

---
+++ 01.ServiceLayer/API.REST/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace API.REST.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ 01.ServiceLayer/API.REST/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace API.REST.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
